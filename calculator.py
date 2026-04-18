58596d13fdbc93a7c22947eebbe7e88df0c8ed17
enaux <enaughton7@protonmail.com>
2026-04-18 16:38:24 +0100

    Add logic.

---
+++ calculator.py
@@ -0,0 +1,32 @@
+# Annual Leave Calculator for HSE Nurses
+# -----------------------------------------------
+
+# Standard Year: 1st April to 31st March
+# Standard Week: 37.5 hours
+# Bank Holiday Allowance: 10 days
+
+# ANNUAL LEAVE ENTITLEMENT
+# -----------------------------------------------
+# Staff Nurses:
+# - 0-5 years of service: 24 days
+# - 6-10 years of service: 25 days
+# - 10+ years of service: 27 days
+# -----------------------------------------------
+# Clinical Nurse Managers:
+# - 0-5 years of service: 25 days
+# - 6-10 years of service: 26 days
+# - 10+ years of service: 28 days
+# -----------------------------------------------
+
+# LOGIC:
+# -----------------------------------------------
+# Select staff member type
+# Enter start date
+# Get weekly hours
+# Calculate percentage of year worked
+# Calculate percentage of week worked
+# Calculate pro-rata annual leave entitlement
+# Calculate annual leave hours
+# -----------------------------------------------
+# FUTURE IMPROVEMENTS:
+# Get bank holiday dates
